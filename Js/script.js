6c2eae8ca7e49536dc8fe260d0e25d68a0f6814b
Dalia Martel~ <dali.martel22@gmail.com>
2023-11-25 07:35:28 -0300

    Prueba de si sirve

--- Js/script.js
+++ Js/script.js
@@ -2,7 +2,15 @@
 
 
 let sinLucro ="Sitio web sin fines de lucro." ;
+let alertaMostradaSinLucro = sessionStorage.getItem('alertaMostradaSinLucro');
+
+if (!alertaMostradaSinLucro) {
 alert (sinLucro);
+sessionStorage.setItem("alertaMostradaSinLucro", "true");
+}
+
+
+
 
 // Método para obtener el elemento body
 let body = document.querySelector("body");
@@ -22,29 +30,38 @@ function cambiarColor() {
 // Pregunta de edad
 let numero = 18;
 let edad;
+let alertaMostrada = localStorage.getItem("alertaMostrada"); // para que no se muestre siempre las alertas y el prompt
 
-while(true) {
-    let input= prompt("¿Cuántos años tenes?");
+if (!alertaMostrada){ 
+ while(true) {
+     let input= prompt("¿Cuántos años tenes?");
 
   if (!isNaN(input)){
      edad = parseInt(input, 10);
-    console.log("Edad ingresada:", edad);
-    break;
-  } 
+     console.log("Edad ingresada:", edad);
+ break;
+} 
+
   else { 
     alert("Por favor, ingrese un número válido.");
-    
-  } }
-
-
-
-if( !isNaN(edad) && edad >= numero) {
- alert ("Podes dejar tus datos para pedir mas información.");
+  }
+}
+localStorage.setItem("alertaMostrada", "true");
 }
 
- else{ 
-     alert("Lo siento, debes ser mayor de 18 años para recibir mas información.");
- }
+
+let alertaFinalMostrada = localStorage.getItem("alertaFinalMostrada");// Guarda en localStorage que la alerta final ya se mostró en alguna sesión anterior
+
+
+if (!alertaFinalMostrada && !isNaN(edad) && edad >= numero) {
+    alert("Podes dejar tus datos para pedir más información.");
+    localStorage.setItem("alertaFinalMostrada", "true");
+    
+} else if (alertaFinalMostrada) {
+    alert("Lo siento, debes ser mayor de 18 años para recibir más información.");
+}
+
+
 
 
 
