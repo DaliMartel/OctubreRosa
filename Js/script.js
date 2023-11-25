2304e6b124efdde58386a0efd58905c40d7cfc93
Dalia Martel~ <dali.martel22@gmail.com>
2023-11-25 06:34:06 -0300

    Nuevos alementos

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,57 @@
-let a = document.getElementById("Salir");
+//Funciones, metodos, condicionales
 
+
+let sinLucro ="Sitio web sin fines de lucro." ;
+alert (sinLucro);
+
+// Método para obtener el elemento body
 let body = document.querySelector("body");
+document.body.style.backgroundColor = "pink" ; // Cambiar el color de fondo del body a rosa
 
-body.style.backgroundColor = "pink";
+
+
+//funciones
+
+function cambiarColor() {
+  let boton = document.getElementById("Enviar");
+  let nuevoColor =  "pink";
+  boton.style.backgroundColor = nuevoColor;
+}
+
+
+// Pregunta de edad
+let numero = 18;
+let edad;
+
+while(true) {
+    let input= prompt("¿Cuántos años tenes?");
+
+  if (!isNaN(input)){
+     edad = parseInt(input, 10);
+    console.log("Edad ingresada:", edad);
+    break;
+  } 
+  else { 
+    alert("Por favor, ingrese un número válido.");
+    
+  } }
+
+
+
+if( !isNaN(edad) && edad >= numero) {
+ alert ("Podes dejar tus datos para pedir mas información.");
+}
+
+ else{ 
+     alert("Lo siento, debes ser mayor de 18 años para recibir mas información.");
+ }
+
+
+
+
+
+
+
+
+
 
