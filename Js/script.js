4b0ae047acbba1d1bb33dd29b351bbcb600a6061
Dalia Martel~ <dali.martel22@gmail.com>
2023-11-25 15:02:18 -0300

    arreglos para que alert no aparezca siempre

--- Js/script.js
+++ Js/script.js
@@ -1,8 +1,8 @@
 //Funciones, metodos, condicionales
 
-
 let sinLucro ="Sitio web sin fines de lucro." ;
 let alertaMostradaSinLucro = sessionStorage.getItem('alertaMostradaSinLucro');
+
 
 if (!alertaMostradaSinLucro) {
 alert (sinLucro);
@@ -28,47 +28,37 @@ function cambiarColor() {
 
 
 // Pregunta de edad
+
 let numero = 18;
 let edad;
-let alertaMostrada = localStorage.getItem("alertaMostrada"); // para que no se muestre siempre las alertas y el prompt
+
+let alertaMostrada = sessionStorage.getItem("alertaMostrada");
 
 if (!alertaMostrada){ 
- while(true) {
-     let input= prompt("¿Cuántos años tenes?");
+while(true) {
+    let input= prompt("¿Cuántos años tenes?");
 
   if (!isNaN(input)){
      edad = parseInt(input, 10);
-     console.log("Edad ingresada:", edad);
- break;
-} 
-
+    console.log("Edad ingresada:", edad);
+    break;
+  } 
   else { 
     alert("Por favor, ingrese un número válido.");
-  }
-}
-localStorage.setItem("alertaMostrada", "true");
-}
-
-
-let alertaFinalMostrada = localStorage.getItem("alertaFinalMostrada");// Guarda en localStorage que la alerta final ya se mostró en alguna sesión anterior
-
-
-if (!alertaFinalMostrada && !isNaN(edad) && edad >= numero) {
-    alert("Podes dejar tus datos para pedir más información.");
-    localStorage.setItem("alertaFinalMostrada", "true");
     
-} else if (alertaFinalMostrada) {
+  } }
+sessionStorage.setItem("alertaMostrada", "true");
+}
+
+let alertaFinalMostrada = localStorage.getItem("alertaFinalMostrada");
+
+
+
+if (!alertaFinalMostrada  && !isNaN(edad) && edad >= numero) {
+    alert("Podes dejar tus datos para pedir más información.");
+
+// Guardar en localStorage que la alerta final ya se mostró en alguna sesión anterior
+    localStorage.setItem("alertaFinalMostrada", 'true');
+} else if (!alertaFinalMostrada) {
     alert("Lo siento, debes ser mayor de 18 años para recibir más información.");
 }
-
-
-
-
-
-
-
-
-
-
-
-
